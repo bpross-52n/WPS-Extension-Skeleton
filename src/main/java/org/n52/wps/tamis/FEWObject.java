be9110af017e8f19c36582ce1b34f67661dd5e23
Benjamin Pross <b.pross@52north.org>
2016-08-23 14:11:02 +0200

    Add object to map Talsim in-/outputs

--- src/main/java/org/n52/wps/tamis/FEWObject.java
+++ src/main/java/org/n52/wps/tamis/FEWObject.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class FEWObject {
 
     private String type;
-
+    
     private String locationId;
 
     private String parameterId;
@@ -30,4 +30,127 @@ public class FEWObject {
 
     private List<Event> eventList;
 
+    public FEWObject(){
+        
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public FEWObject setType(String type) {
+        this.type = type;
+        return this;
+    }
+
+    public String getLocationId() {
+        return locationId;
+    }
+
+    public FEWObject setLocationId(String locationId) {
+        this.locationId = locationId;
+        return this;
+    }
+
+    public String getParameterId() {
+        return parameterId;
+    }
+
+    public FEWObject setParameterId(String parameterId) {
+        this.parameterId = parameterId;
+        return this;
+    }
+
+    public String getTimeStepUnit() {
+        return timeStepUnit;
+    }
+
+    public FEWObject setTimeStepUnit(String timeStepUnit) {
+        this.timeStepUnit = timeStepUnit;
+        return this;
+    }
+
+    public String getTimeStepMultiplier() {
+        return timeStepMultiplier;
+    }
+
+    public FEWObject setTimeStepMultiplier(String timeStepMultiplier) {
+        this.timeStepMultiplier = timeStepMultiplier;
+        return this;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public FEWObject setStartDate(String startDate) {
+        this.startDate = startDate;
+        return this;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public FEWObject setStartTime(String startTime) {
+        this.startTime = startTime;
+        return this;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public FEWObject setEndDate(String endDate) {
+        this.endDate = endDate;
+        return this;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public FEWObject setEndTime(String endTime) {
+        this.endTime = endTime;
+        return this;
+    }
+
+    public String getMissVal() {
+        return missVal;
+    }
+
+    public FEWObject setMissVal(String missVal) {
+        this.missVal = missVal;
+        return this;
+    }
+
+    public String getStationName() {
+        return stationName;
+    }
+
+    public FEWObject setStationName(String stationName) {
+        this.stationName = stationName;
+        return this;
+    }
+
+    public String getUnits() {
+        return units;
+    }
+
+    public FEWObject setUnits(String units) {
+        this.units = units;
+        return this;
+    }
+
+    public List<Event> getEventList() {
+        return eventList;
+    }
+
+    public FEWObject setEventList(List<Event> eventList) {
+        this.eventList = eventList;
+        return this;
+    }
+
+    
+    
 }
